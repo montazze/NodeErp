e57827181faadf9341dcc894a38cea7402e58464
steve mangelschots <smangelschots@hotmail.com>
2012-10-12 02:17:59 +0200

    update views to work jtable

---
+++ public/js/lib/jdialog.js
@@ -0,0 +1,19 @@
+/**
+ * Company: OfficeSoft
+ * User: smangelschots
+ * Date: 12/10/12
+ * Time: 00:33
+ */
+$(function(){
+
+    $('<div>').attr({
+             id:  "editDialog"
+    }).appendTo('.widgetPanel');
+
+
+    $('<div>').attr({
+        id: "contentArea"
+    }).appendTo('#editDialog');
+
+
+})
---
+++ public/js/viewmodel/contact.js
@@ -0,0 +1,90 @@
+/**
+ * Company: OfficeSoft
+ * User: smangelschots
+ * Date: 22/09/12
+ * Time: 00:20
+ */
+$(function(){
+
+        nerp.Contact = function(){
+            var self = this;
+            self.id = ko.observable();
+            self.name = ko.observable();
+            self.surname = ko.observable();
+            self.street = ko.observable();
+            self.country = ko.observable();
+        };
+
+
+        nerp.vm = function(){
+
+           contacts = ko.observableArray([]),
+
+           loadContactsCallback = function(json){
+               $.each(json,function(i,p){
+                    contacts.push(new nerp.Contact()
+                        .id(p._id)
+                        .name(p.name)
+                        .surname(p.surname)
+                        .street(p.street)
+                        .country(p.country)
+                   );
+
+               });
+
+           },
+
+            loadContacts = function(){
+                $.ajax({
+                            url: "/contact.json",
+                            type: "GET",
+                            data: ko.toJSON(null),
+                            dataType: "json",
+                            contentType: "application/json",
+                            success: function (json) {
+                                loadContactsCallback(json);
+                            }
+                });
+            }
+        return{
+            contacts: contacts,
+            loadContactsCallback: loadContactsCallback,
+            loadContacts: loadContacts
+        }
+
+        } ();
+
+
+        nerp.vm.loadContacts();
+        ko.applyBindings(nerp.vm);
+
+
+
+
+$(document).on("click",".edit-click",function(){
+       $("#contentArea").load('/contact/new');
+       $("#editDialog").dialog({
+           modal: true,
+           height: 350,
+           width: 400
+
+       });
+
+    $()
+   });
+
+
+$(".viewTable").dataTable();
+
+
+});
+
+
+
+
+
+
+
+
+
+
